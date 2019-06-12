74698bfede69ee0141168ddddcaed45adac25d48
Vince94m <cypetdesigns@gmail.com>
2019-06-12 10:03:33 +0200

    fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'this is where the error is',
     user : 'postgres',
     ssl: true,
     password : 'VTMNKCMT2real',
